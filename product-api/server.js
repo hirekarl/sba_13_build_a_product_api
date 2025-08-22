7b4decdb795549a018d8f2c87d07db7924167518
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-22 10:07:04 -0400

    Built out server, moved constants to utils.

--- product-api/server.js
+++ product-api/server.js
@@ -1,3 +1,18 @@
+const express = require("express")
 const connect = require("./db/connection")
+const productRoutes = require("./routes/productRoutes")
+
+const { STATIC_ROOT, PORT } = require("./utils")
+
+const app = express()
+
+app.use(express.static(STATIC_ROOT))
+app.use(express.json())
+
+app.use("/api/products", productRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}.`)
+})
 
 connect()
--- product-api/utils/index.js
+++ product-api/utils/index.js
@@ -2,7 +2,8 @@ const path = require("path")
 
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") })
 
+const STATIC_ROOT = path.resolve(__dirname, "../public")
 const PORT = process.env.PORT
 const MONGO_URI = process.env.MONGO_URI
 
-module.exports = { PORT, MONGO_URI }
+module.exports = { STATIC_ROOT, PORT, MONGO_URI }
