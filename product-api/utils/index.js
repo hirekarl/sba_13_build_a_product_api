09528d76a7c83b7f6e82354acebe8efbf01ec687
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-22 08:44:48 -0400

    Added util to hold envvars.

---
+++ product-api/utils/index.js
@@ -0,0 +1,8 @@
+const path = require("path")
+
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") })
+
+const PORT = process.env.PORT
+const MONGO_URI = process.env.MONGO_URI
+
+module.exports = { PORT, MONGO_URI }
