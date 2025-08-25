57c29fc89df20e8249fd8726e9ff54500b885670
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-25 12:36:08 -0400

    Added mock data.

---
+++ product-api/data/products.js
@@ -0,0 +1,93 @@
+const products = [
+  {
+    name: "Duke's Mayo",
+    description: "The BEST mayonaise in the world. Fight the power, not me.",
+    price: 4.99,
+    category: "Groceries",
+    inStock: true,
+    tags: ["dukes", "mayonaise", "sandwich", "happiness"],
+  },
+  {
+    name: "JigglyPuff",
+    description:
+      "The BEST Pokemon in the world. An icon, the moment, she is everything. Accompanied by a mic/marker.",
+    price: 100.99,
+    category: "Electronics",
+    inStock: true,
+    tags: ["sing", "Pokemon", "hmmph", "scribble"],
+  },
+  {
+    name: "Colgate Jr",
+    description:
+      "Sparkles in your toothpaste, in an awesome shape, starry starry. Its got an awesome taste.",
+    price: 6.99,
+    category: "Health",
+    inStock: true,
+    tags: ["teeth", "stinky breath", "brush", "bubblegum"],
+  },
+  {
+    name: "Hydrox Cookies",
+    description:
+      "Better than Oreos, only you don't know that because capitalism. Amazing with milk. Put 10 in a glass, pour milk over and let soak then eat with a spoon.",
+    price: 6.99,
+    category: "Groceries",
+    inStock: true,
+    tags: ["cookie", "underrated", "original", "Keebler"],
+  },
+  {
+    name: "EverReady Batteries C 12 pk",
+    description:
+      "Better than Energizer, but you wouldn't know that because capitalism. There was a black cat on the 9V battery that I may have licked. Judge someone else.",
+    price: 30.99,
+    category: "Electronics",
+    inStock: true,
+    tags: ["no bunny", "lick batteries", "silver", "shock of your life"],
+  },
+  {
+    name: "McDLT",
+    description:
+      "Keep your hot side hot, and your cool side cool. Clearly one of McDonald's best products, BUT in the 80s they used too much styrofoam.",
+    price: 12.99,
+    category: "Prepared Foods",
+    inStock: true,
+    tags: ["no whoppers", "lettuce", "tomato", "bun"],
+  },
+  {
+    name: "Cabbage Patch doll",
+    description:
+      "One of the first viral toys of the 80s. I had one because I'm elite. The hair is made of yarn, and you get a birth certificate for your doll.The original dolls were all cloth.",
+    price: 50.99,
+    category: "Toys",
+    inStock: true,
+    tags: ["doll", "not Elmo", "hand sewn", "big head"],
+  },
+  {
+    name: "Rubik's Cube Original",
+    description:
+      "Another viral toy from the 80s. The cube has 86 sides with many different colored panels that you need to rearrange so that each side of the cube contains all one color. Good luck. I just peeled the stickers",
+    price: 25.99,
+    category: "Toys",
+    inStock: true,
+    tags: ["nerd", "game", "puzzle", "why try"],
+  },
+  {
+    name: "The Original Domino's Pizza",
+    description:
+      "A hot pepperoni pizza made just like they made it in the 80's and 90's. Avoid the Noid.",
+    price: 18.99,
+    category: "Prepared Foods",
+    inStock: true,
+    tags: ["cheese", "delivered", "noid", "pepperoni"],
+  },
+  {
+    name: "Original Listerine",
+    description:
+      "This kills the germs that cause bad breath, but at a cost... Your soul! This will burn your mouth so badly, your ancestors breath will be fresh!",
+    price: 9.99,
+    category: "Health",
+    inStock: false,
+    tags: ["burn", "stinky breath", "why me", "volcano"],
+  },
+]
+
+module.exports = products
