f218294836a32d1acfd4104be2545a027ebaec5e
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-25 13:32:20 -0400

    Added '|| 3001' to PORT in utils.

--- product-api/utils/index.js
+++ product-api/utils/index.js
@@ -3,7 +3,7 @@ const path = require("path")
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") })
 
 const STATIC_ROOT = path.resolve(__dirname, "../public")
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 const MONGO_URI = process.env.MONGO_URI
 
 module.exports = { STATIC_ROOT, PORT, MONGO_URI }
